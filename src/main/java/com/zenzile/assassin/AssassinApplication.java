cc768400c58bdefec2fee7f83f40b83f694fdea8
zenzilesihle <sihlezen10@gmail.com>
2018-03-14 03:57:50 +0200

    - Initial commit

---
+++ src/main/java/com/zenzile/assassin/AssassinApplication.java
@@ -0,0 +1,13 @@
+package com.zenzile.assassin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssassinApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AssassinApplication.class, args);
+	}
+}
