808154e12df6db1558637d2b5257e6c71205c5a9
sihlezenzile@gmail.com <sihle@skywalkinnovations.co.za>
2018-04-26 17:50:08 +0200

    - Fininshed admin controller

--- src/main/java/com/zenzile/assassin/rest/AdminController.java
+++ src/main/java/com/zenzile/assassin/rest/AdminController.java
@@ -1,16 +1,60 @@
 package com.zenzile.assassin.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.zenzile.assassin.model.Admin;
+import com.zenzile.assassin.repository.AdminRepository;
+import com.zenzile.assassin.service.AdminService;
+import org.assertj.core.util.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus( HttpStatus.CREATED )
+    @ResponseBody
+    public Admin addAdmin(@RequestBody Admin admin){
+        Preconditions.checkNotNull( admin );
+
+        //need to handle null
+        return adminService.registerAdmin(admin);
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-    public String viewAdmin() {
-        System.out.println("IT CAME HERE");
-        return "Assassin Is Up And Running";
+    @ResponseBody
+    public List<Admin> findAll(){
+        Iterable<Admin> admins = adminRepository.findAll();
+        List<Admin> adminList = new ArrayList<>();
+
+        while(admins.iterator().hasNext()) {
+            adminList.add(admins.iterator().next());
+        }
+
+        return adminList;
+    }
+
+    @RequestMapping(value = "/{email}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Admin findByEmail(@PathVariable( "id" ) String email){
+
+        return adminService.findByEmail(email);
+    }
+
+    @RequestMapping( value = "/{id}", method = RequestMethod.PUT )
+    @ResponseStatus(HttpStatus.OK)
+    public void update( @PathVariable( "id" ) Long id, @RequestBody Admin admin ){
+        Preconditions.checkNotNull(admin);
+        Preconditions.checkNotNull(adminRepository.findById(admin.getId()));
+        adminService.updateAdmin(admin);
     }
 }
--- src/test/java/com/zenzile/assassin/rest/AdminControllerTest.java
+++ src/test/java/com/zenzile/assassin/rest/AdminControllerTest.java
@@ -1,9 +1,29 @@
 package com.zenzile.assassin.rest;
 
 import com.zenzile.assassin.AssassinApplicationTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AdminControllerTest extends AssassinApplicationTests {
 
+    @Test
+    public void test1AddAdmin() {
+
+    }
+
+    @After
+    public void cleanUp(){
+
+    }
+
+    @Before
+    public void setUp() {
+
+    }
 }
